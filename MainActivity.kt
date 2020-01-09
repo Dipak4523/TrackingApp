c05f44d03c866a850fdaa0f2aef1f00998ac5930
Dipak <46080330+Dipak4523@users.noreply.github.com>
2020-01-09 22:21:31 +0530

    Add files via upload

---
+++ MainActivity.kt
@@ -0,0 +1,110 @@
+package com.app.trackingapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationResult
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
+
+class MainActivity : AppCompatActivity() {
+
+
+    companion object {
+        private const val MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 2200
+    }
+
+    private lateinit var googleMap: GoogleMap
+    private lateinit var locationProviderClient: FusedLocationProviderClient
+    private lateinit var locationRequest: LocationRequest
+    private lateinit var locationCallback: LocationCallback
+    private var locationFlag = true
+    private var driverOnlineFlag = false
+    private var currentPositionMarker: Marker? = null
+
+    private val googleMapHelper = GoogleMapHelper()
+    //private val firebaseHelper = FirebaseHelper("0000")
+
+    //private val markerAnimationHelper = MarkerAnimationHelper()
+    //private val uiHelper = UiHelper()
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //load google map
+        val mapFragment: SupportMapFragment =
+            supportFragmentManager.findFragmentById(R.id.supportMap) as SupportMapFragment
+
+        mapFragment.getMapAsync { googleMap = it }
+
+        createLocationCallback()
+        //(locationCallback)
+
+    }
+
+    private fun createLocationCallback() {
+        locationCallback = object : LocationCallback() {
+            override fun onLocationResult(locationResult: LocationResult?) {
+                super.onLocationResult(locationResult)
+                if (locationResult!!.lastLocation == null) return
+                val latLng = LatLng(
+                    locationResult.lastLocation.latitude,
+                    locationResult.lastLocation.longitude
+                )
+                // Log.e("Location", latLng.latitude.toString() + " , " + latLng.longitude)
+                if (locationFlag) {
+                    locationFlag = false
+                    animateCamera(latLng)
+                }
+                //if (driverOnlineFlag) firebaseHelper.updateDriver(Driver(lat = latLng.latitude, lng = latLng.longitude))
+                showOrAnimateMarker(latLng)
+            }
+        }
+    }
+
+    private fun showOrAnimateMarker(latLng: LatLng) {
+        if (currentPositionMarker == null)
+            currentPositionMarker =
+                googleMap.addMarker(googleMapHelper.getDriverMarkerOptions(latLng))
+        //else
+        // animateMarkerToGB(currentPositionMarker!!, latLng, LatLngInterpolator.Spherical())
+    }
+
+    private fun animateCamera(latLng: LatLng) {
+        val cameraUpdate = googleMapHelper.buildCameraUpdate(latLng)
+        googleMap.animateCamera(cameraUpdate, 10, null)
+    }
+
+
+    /*fun animateMarkerToGB(marker: Marker, finalPosition: LatLng, latLngInterpolator: LatLngInterpolator) {
+        val startPosition = marker.position
+        val handler = Handler()
+        val start = SystemClock.uptimeMillis()
+        val interpolator = AccelerateDecelerateInterpolator()
+        val durationInMs = 2000f
+        handler.post(object : Runnable {
+            var elapsed: Long = 0
+            var t: Float = 0.toFloat()
+            var v: Float = 0.toFloat()
+            override fun run() {
+                // Calculate progress using interpolator
+                elapsed = SystemClock.uptimeMillis() - start
+                t = elapsed / durationInMs
+                v = interpolator.getInterpolation(t)
+                marker.position = latLngInterpolator.interpolate(v, startPosition, finalPosition)
+                // Repeat till progress is complete.
+                if (t < 1) {
+                    // Post again 16ms later.
+                    handler.postDelayed(this, 16)
+                }
+            }
+        })
+    }*/
+
+}
